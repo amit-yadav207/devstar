c56dde4f2d6c06dfaa0fc82e896d75a4599ec720
Aniket <aniketagrahari60@gmail.com>
2024-06-28 15:03:58 +0530

    add camera, download, record, stop icon

--- src/routes/(tools)/camera-recorder/icons/DownloadIcon.svelte
+++ src/routes/(tools)/camera-recorder/icons/DownloadIcon.svelte
@@ -1,6 +1,6 @@
 <script>
     export let className = '';
-  </script>
+</script>
   
   <svg class={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M5 20H19" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> 
--- src/routes/(tools)/camera-recorder/icons/RecordIcon.svelte
+++ src/routes/(tools)/camera-recorder/icons/RecordIcon.svelte
@@ -1,6 +1,6 @@
 <script>
     export let className = '';
-  </script>
+</script>
   
   <svg class={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="12" cy="12" r="10" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></circle> 
